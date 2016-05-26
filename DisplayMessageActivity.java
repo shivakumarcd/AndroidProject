c5c92ea5a1b7c7b3f7fb5e0f5fe463864c320a65
Shivakumar Doddamani <mailtoshivoo@gmail.com>
2016-05-25 19:40:12 -0700

    My first commit

---
+++ DisplayMessageActivity.java
@@ -0,0 +1,69 @@
+package com.example.shivs.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display_message_activity_layout);
+       /* Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null)
+                        .show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
+
+        Bitmap image = (Bitmap)intent.getParcelableExtra(MyActivity.EXTRA_MESSAGE_IMAGE);
+
+        System.out.println("************in DisplayAcitivity and value of color = " + image.getPixel(100,100));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_display_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ MyActivity.java
@@ -0,0 +1,95 @@
+package com.example.shivs.myapplication;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MyActivity extends Activity {
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    public final static String EXTRA_MESSAGE_IMAGE = "com.mycompany.myfirstapp.MESSAGE_IMAGE";
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final ImageView mImageView = null;
+    View view = null;
+    Bitmap imageBitmap = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        //disabling image capture button if camera is not available in device
+        PackageManager packageMgr = getPackageManager();
+        if (!packageMgr.hasSystemFeature(PackageManager.FEATURE_CAMERA))
+        {
+            Button imageCaptureButton = (Button) findViewById(R.id.capture_image);
+            imageCaptureButton.setEnabled(false);
+        }
+    }
+
+    public void captureNewSyllabus(View view)
+    {
+        //saving view in class variable to use it later to navigate to next activity in onActivityResult()
+        this.view= view;
+
+        //calling function to capture new image
+        dispatchTakePictureIntent();
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+           imageBitmap = (Bitmap) extras.get("data");
+            //mImageView.setImageBitmap(imageBitmap);
+            receiveMsg(this.view);
+        }
+    }
+    public void receiveMsg(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String message = "Took image successfully";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_MESSAGE_IMAGE, imageBitmap);
+        startActivity(intent);
+        System.out.println("****************** pixel value"+imageBitmap.getPixel(100,100));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
